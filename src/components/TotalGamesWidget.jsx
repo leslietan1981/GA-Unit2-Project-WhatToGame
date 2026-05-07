23d044547a3571f55abdb8bc233cdad0a99c763d
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-07 15:00:32 +0800

    Add navigate to manage wishlist

--- src/components/TotalGamesWidget.jsx
+++ src/components/TotalGamesWidget.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import css from "../styles/Widget.module.css";
+import { routerPaths } from "../utils/routerPathUtil";
 
 const TotalGamesWidget = (props) => {
   return (
-    <div className={`${css["widget-tile"]} ${css["w1"]} ${css["h1"]}`}>
+    <div
+      className={`${css["widget-tile"]} ${css["w1"]} ${css["h1"]}`}
+      onClick={props.onClick && (() => props.onClick(routerPaths.manageWishlist))}
+    >
       <div className={css["emphasis"]}>{props.total}</div>
       <div className={css["title"]}>games in wishlist</div>
     </div>
