2d776cc4596f38eec8da92f40880dbb7dfd687d6
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-08 02:22:39 +0800

    Add link to OP Monster game

--- src/easteregg/OPMonWidget.jsx
+++ src/easteregg/OPMonWidget.jsx
@@ -4,7 +4,10 @@ import OPMGrid from "./OPMGrid";
 
 const OPMonWidget = () => {
   return (
-    <div className={`${css["widget-tile"]} ${css["w3"]} ${css["h2"]}`}>
+    <div
+      className={`${css["widget-tile"]} ${css["w3"]} ${css["h2"]}`}
+      onClick={() => window.open("https://leslietan1981.github.io/GA-Unit1-Project-OPmon/", "_blank", "noreferrer")}
+    >
       <OPMGrid />
     </div>
   );
